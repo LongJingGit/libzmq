f5dc00590a4e463336e9c12d0b27dca47847d287
jinglong <jinglong@eastmoney.com>
2021-12-16 14:30:31 +0800

    other# format the code

--- src/sub.cpp
+++ src/sub.cpp
@@ -31,8 +31,8 @@
 #include "sub.hpp"
 #include "msg.hpp"
 
-zmq::sub_t::sub_t (class ctx_t *parent_, uint32_t tid_, int sid_) :
-    xsub_t (parent_, tid_, sid_)
+zmq::sub_t::sub_t(class ctx_t *parent_, uint32_t tid_, int sid_)
+    : xsub_t(parent_, tid_, sid_)
 {
     options.type = ZMQ_SUB;
 
@@ -41,15 +41,12 @@ zmq::sub_t::sub_t (class ctx_t *parent_, uint32_t tid_, int sid_) :
     options.filter = true;
 }
 
-zmq::sub_t::~sub_t ()
-{
-}
+zmq::sub_t::~sub_t() {}
 
-int zmq::sub_t::xsetsockopt (int option_,
-                             const void *optval_,
-                             size_t optvallen_)
+int zmq::sub_t::xsetsockopt(int option_, const void *optval_, size_t optvallen_)
 {
-    if (option_ != ZMQ_SUBSCRIBE && option_ != ZMQ_UNSUBSCRIBE) {
+    if (option_ != ZMQ_SUBSCRIBE && option_ != ZMQ_UNSUBSCRIBE)
+    {
         errno = EINVAL;
         return -1;
     }
@@ -57,27 +54,30 @@ int zmq::sub_t::xsetsockopt (int option_,
     //  Create the subscription message.
     msg_t msg;
     int rc;
-    const unsigned char *data = static_cast<const unsigned char *> (optval_);
-    if (option_ == ZMQ_SUBSCRIBE) {
-        rc = msg.init_subscribe (optvallen_, data);
-    } else {
-        rc = msg.init_cancel (optvallen_, data);
+    const unsigned char *data = static_cast<const unsigned char *>(optval_);
+    if (option_ == ZMQ_SUBSCRIBE)
+    {
+        rc = msg.init_subscribe(optvallen_, data);
     }
-    errno_assert (rc == 0);
+    else
+    {
+        rc = msg.init_cancel(optvallen_, data);
+    }
+    errno_assert(rc == 0);
 
     //  Pass it further on in the stack.
-    rc = xsub_t::xsend (&msg);
-    return close_and_return (&msg, rc);
+    rc = xsub_t::xsend(&msg);
+    return close_and_return(&msg, rc);
 }
 
-int zmq::sub_t::xsend (msg_t *)
+int zmq::sub_t::xsend(msg_t *)
 {
     //  Override the XSUB's send.
     errno = ENOTSUP;
     return -1;
 }
 
-bool zmq::sub_t::xhas_out ()
+bool zmq::sub_t::xhas_out()
 {
     //  Override the XSUB's send.
     return false;
