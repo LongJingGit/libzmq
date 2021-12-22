2f5729bf5a10d62d209384673bc24a8098a2f749
jinglong <jinglong@eastmoney.com>
2021-12-22 10:30:16 +0800

    other# format the code

--- src/trie.hpp
+++ src/trie.hpp
@@ -35,38 +35,31 @@
 #include "macros.hpp"
 #include "stdint.hpp"
 
-namespace zmq
-{
+namespace zmq {
 class trie_t
 {
-  public:
-    trie_t ();
-    ~trie_t ();
+public:
+    trie_t();
+    ~trie_t();
 
     //  Add key to the trie. Returns true if this is a new item in the trie
     //  rather than a duplicate.
-    bool add (unsigned char *prefix_, size_t size_);
+    bool add(unsigned char *prefix_, size_t size_);
 
     //  Remove key from the trie. Returns true if the item is actually
     //  removed from the trie.
-    bool rm (unsigned char *prefix_, size_t size_);
+    bool rm(unsigned char *prefix_, size_t size_);
 
     //  Check whether particular key is in the trie.
-    bool check (const unsigned char *data_, size_t size_) const;
+    bool check(const unsigned char *data_, size_t size_) const;
 
     //  Apply the function supplied to each subscription in the trie.
-    void apply (void (*func_) (unsigned char *data_, size_t size_, void *arg_),
-                void *arg_);
+    void apply(void (*func_)(unsigned char *data_, size_t size_, void *arg_), void *arg_);
 
-  private:
-    void apply_helper (unsigned char **buff_,
-                       size_t buffsize_,
-                       size_t maxbuffsize_,
-                       void (*func_) (unsigned char *data_,
-                                      size_t size_,
-                                      void *arg_),
-                       void *arg_) const;
-    bool is_redundant () const;
+private:
+    void apply_helper(unsigned char **buff_, size_t buffsize_, size_t maxbuffsize_, void (*func_)(unsigned char *data_, size_t size_, void *arg_),
+        void *arg_) const;
+    bool is_redundant() const;
 
     uint32_t _refcnt;
     unsigned char _min;
@@ -78,8 +71,8 @@ class trie_t
         class trie_t **table;
     } _next;
 
-    ZMQ_NON_COPYABLE_NOR_MOVABLE (trie_t)
+    ZMQ_NON_COPYABLE_NOR_MOVABLE(trie_t)
 };
-}
+} // namespace zmq
 
 #endif
